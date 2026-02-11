01820b88b8ff494beea7341c56443485ed2dcc0e
Abhishekn sen <abhishek@innovatiqconsulting.com>
2026-02-11 18:36:01 +0530

    Implement LazyLoading

---
+++ lazyLoading/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>React Lazy Loading Example</h1>
+
+      <nav style={{ display: "flex", gap: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <hr />
+
+      <Suspense fallback={<h2> Loading Page...</h2>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ lazyLoading/src/pages/Contact.jsx
@@ -0,0 +1,5 @@
+const Contact = () => {
+  return <h2> Contact Page Loaded</h2>;
+};
+
+export default Contact;
